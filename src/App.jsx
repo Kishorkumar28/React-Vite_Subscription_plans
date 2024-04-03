e14cfeb4295b093afa676fa4ab63c7adb8adf584
Kishorkumar28 <kishor28092002@gmail.com>
2024-04-03 16:45:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+import Square from "./Components/Square";
+
+function App() {
+  const data = [
+    
+    {
+      title: "FREE",
+      price:" $ 0/month",
+      color: "white",
+      features: [
+        { name: "5 Users", isAvailable: true },
+        { name: "50GB Storage", isAvailable: true },
+        { name: "50GB Storage", isAvailable: true },
+        { name: "Communty Projects", isAvailable: true },
+        { name: "Unlimited Private Projects", isAvailable: false},
+        { name: "Dedicated Phone Support", isAvailable: false},
+        { name: "Free Subdoamin", isAvailable: false},
+        { name: "Monthly Status Reports", isAvailable: false},
+      ],
+    },
+    {
+      title: "PLUS",
+      price:" $ 9/month",
+      color: "white",
+      features: [
+        { name: "5 Users", isAvailable: true },
+        { name: "50GB Storage", isAvailable: true },
+        { name: "50GB Storage", isAvailable: true },
+        { name: "Communty Projects", isAvailable: true },
+        { name: "Unlimited Private Projects", isAvailable: true},
+        { name: "Dedicated Phone Support", isAvailable: true},
+        { name: "Free Subdoamin", isAvailable: true},
+        { name: "Monthly Status Reports", isAvailable: false},
+      ],
+    },
+    {
+      title: "PRO",
+      price:" $ 49/month",
+      color: "white",
+      features: [
+        { name: "5 Users", isAvailable: true },
+        { name: "50GB Storage", isAvailable: true },
+        { name: "50GB Storage", isAvailable: true },
+        { name: "Communty Projects", isAvailable: true },
+        { name: "Unlimited Private Projects", isAvailable: true},
+        { name: "Dedicated Phone Support", isAvailable: true},
+        { name: "Free Subdoamin", isAvailable: true},
+        { name: "Monthly Status Reports", isAvailable: true},
+      ],
+    },
+
+    
+  ];
+
+  return (
+    <div className="parent-container">
+      {data.map((element, index) => (
+        <Square
+          key={index}
+          width={450}
+          height={750}
+          background={element.color}
+          price={element.price}
+          message={element.title}
+          items={element.features}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+// import React from "react";
+// import Square, { Circle } from "./Components/Square";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+
+//   // 1
+//   componentWillMount() {
+//     console.log("Will Mount");
+//   }
+
+//   // 2
+//   componentDidMount() {
+//     console.log("Did mount");
+//   }
+
+//   // 3
+//   componentWillUpdate() {
+//     console.log("Updating");
+//   }
+
+//   // 4
+//   componentDidUpdate() {
+//     console.log("Updated");
+//   }
+
+//   handleDecrement = (e) => {
+//     this.setState({
+//       count: this.state.count - 1,
+//     });
+//   };
+
+//   handleIncrement = (e) => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <button onClick={this.handleDecrement}>-</button>
+//         <p>{this.state.count}</p>
+//         <button onClick={this.handleIncrement}>+</button>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ src/Components/Square.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+
+export default function Square(props = {}) {
+  const { width, height, background,price, message, items } = props;
+  return (
+    <div 
+      style={{
+        width: width,
+        height: height,
+        background: background,
+      }}
+      className="container"
+    >
+      <h1 className="plans">{message}</h1>
+      <h2 className="prices">{price}
+        <span className="price-line"></span>
+      </h2>
+    
+      <h2>
+      <ul>
+        {items.map((element, index) => (
+          <li key={index}>
+            {element.isAvailable ? "✅" : "❌"} {element.name}
+          </li>
+        ))}
+      </ul>
+      </h2>
+      <input className="buttons" type="button" value="Buy"></input>
+    </div>
+  );
+}
+
+
+Square.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  background: PropTypes.string,
+  price: PropTypes.string,
+  message: PropTypes.string,
+  items: PropTypes.array,
+};
